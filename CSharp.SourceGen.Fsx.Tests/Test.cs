dc422b902d6663db88f136eefdba8e0d5f0515a6
dartk <dartk@users.noreply.github.com>
2023-02-16 23:18:17 +0700

    Replaces static generator with incremental

--- CSharp.SourceGen.Fsx.Tests/Test.cs
+++ CSharp.SourceGen.Fsx.Tests/Test.cs
@@ -10,6 +10,8 @@ public class Tests
     public void CheckGeneratedContent()
     {
         var content = File.ReadAllText("content.txt");
+#pragma warning disable xUnit2000
         Assert.Equal(content, Generated.Content);
+#pragma warning restore xUnit2000
     }
 }
---
+++ CSharp.SourceGen.Fsx/FsxIncrementalGenerator.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+
+
+namespace CSharp.SourceGen.Fsx;
+
+
+[Generator(LanguageNames.CSharp)]
+public class FsxIncrementalGenerator : IIncrementalGenerator
+{
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        var additionalFiles = context.AdditionalTextsProvider
+            .Select(static (file, token) =>
+            {
+                var source = file.GetText(token)?.ToString() ?? string.Empty;
+                return new AdditionalFile(file.Path, source);
+            })
+            .Where(static x => x.IsNotEmpty);
+
+        var scripts = additionalFiles.Where(file =>
+            file.FilePath.EndsWith(".fsx")
+            && file.FileName[0] != '_'
+            && file.IsNotEmpty);
+
+        // Scripts can depend on other additional files. To call the generator on this files' changes
+        // we need to include them into the cache
+        var scriptsWithDependencies = scripts.Combine(additionalFiles.Collect())
+            .Select(static (arg, _) =>
+            {
+                var (script, additionalFiles) = arg;
+                var builder =
+                    ImmutableArray.CreateBuilder<AdditionalFile>(additionalFiles.Length);
+                foreach (var file in additionalFiles)
+                {
+                    if (script.Text.Contains(file.FileName))
+                    {
+                        builder.Add(file);
+                    }
+                }
+
+                return (scriptToExecute: script, builder.ToImmutable());
+            });
+
+        context.RegisterSourceOutput(scriptsWithDependencies, (productionContext, script) =>
+            CompileScript(productionContext, script.scriptToExecute.FilePath));
+    }
+
+
+    private static void CompileScript(SourceProductionContext context, string scriptFile)
+    {
+        var source =
+            $"// Generated from '{Path.GetFileName(scriptFile)}'"
+            + Environment.NewLine
+            + ScriptRunner.Run(scriptFile, context.ReportDiagnostic, context.CancellationToken);
+        context.AddSource(Path.GetFileNameWithoutExtension(scriptFile) + ".g", source);
+    }
+
+
+    private readonly record struct AdditionalFile(string FilePath, string Text)
+    {
+        public string FileName => Path.GetFileName(this.FilePath);
+        public bool IsNotEmpty => !string.IsNullOrWhiteSpace(this.Text);
+    }
+}
